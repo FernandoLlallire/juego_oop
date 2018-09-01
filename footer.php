12d895c4b006f5babc993f588f94686f9c8ff89b
FernandoLlallire <fernandorllallire@gmail.com>
2018-09-01 20:49:06 -0300

    limpie algo lo repetido

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
     <footer class="myFooter">
       <p>Derechos reservados</p>
     </footer>
+    <!-- Librería de jQuery -->
+     <script src="js/jquery-3.3.1.min.js"></script>
+     <!-- Librería de JS de Bootstrap -->
+     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
