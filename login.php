25c25a5f0e67199912ac488fa877c9552921d860
Satwich <omargabgonzalez@gmail.com>
2018-09-03 23:42:35 -0300

    <Centrado del form>

--- login.php
+++ login.php
@@ -1,29 +1,24 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link href="https://fonts.googleapis.com/css?family=Baloo+Tammudu" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Login</title>
-</head>
-<body>
   <?php require_once 'header.php'; ?>
-
-
+  <br>
+  <br>
   <div class="container">
     <div class="row login">
-      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-        <form role="form">
-          <fieldset class="formulario">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-sm-offset-2 col-md-offset-3">
+        <form role="form" class="formulario">
+          <fieldset>
             <h1>Logueate</h1>
             <hr class="colorgraph">
-            <div class="form-group laCajaDeAnto">
-              <input type="email" name="email" class="form-control input-lg" placeholder="E-mail">
-            </div>
-            <div class="form-group laCajaDeAnto">
-              <input type="password" name="password" class="form-control input-lg" placeholder="Contraseña">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                  <input type="email" name="email" class="form-control input-lg" placeholder="E-mail">
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                  <input type="password" name="password" class="form-control input-lg" placeholder="Contraseña">
+                </div>
+              </div>
             </div>
             <hr class="colorgraph"> <!-- agregar colorgraph luego -->
             <div class="row">
@@ -43,5 +38,3 @@
     </div>
   </div>
   <?php require_once 'footer.php'; ?>
-</body>
-</html>
