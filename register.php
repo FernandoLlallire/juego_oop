af34907f897038ef1abc6c5171eb35ac6bae2b22
De Los Santos <maria.de.los.santos@accenture.com>
2018-10-30 17:05:08 -0300

    Save Image

--- register.php
+++ register.php
@@ -23,6 +23,7 @@ if ($_POST) {
   $errors = sanitizateAndValidateDataRegister($_POST, $_FILES);
   if (!$errors){
     $_POST["avatar"] = $_FILES["imagen"];//$_FILES es un array donde estan todos los archivos que subamos, en este caso mandamos todos los datos de nuestra imagen (nombre puesto en el label del input)
+    // SaveImage::uploadImage($_FILES["imagen"]);
     saveUser($_POST);
     if ($rememberMe == true){
       saveCookie($_POST);
