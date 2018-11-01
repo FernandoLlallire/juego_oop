c1942c322d9c1bfbbb489dcc6bb26cf294722c43
FernandoLlallire <fernandorllallire@gmail.com>
2018-10-31 21:59:42 -0300

    se agrega un archivo para pasar de json a mysql pendiente de la parte mysql para guardar

--- jsonToMySql.php
+++ jsonToMySql.php
@@ -1,4 +1,10 @@
-<?php  ?>
+<?php
+if($_POST){
+  $json=file_get_contents("db\\db.json");
+  $obj=json_decode($json);
+  var_dump($obj);//Aca va la funcion de la base de datos a la cual le pasamos el objeto de json
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -8,7 +14,7 @@
   <body>
     <h1>Transferencia de Json a MySql</h1>
     <form action="jsonToMySql.php" method="post">
-      <button type="submit" name="transferir"></button>
+      <button type="submit" name="transferir">Transferir db!</button>
     </form>
   </body>
 </html>
